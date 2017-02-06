38b5a952784bcb7d246b13c94fa3dc60562db8fa
Shirin Tag <taghavi.shirin@gmail.com>
2017-02-06 13:25:51 +0100

    infoWindow first step

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Map, {GoogleApiWrapper, maps, Marker} from 'google-maps-react';
-import mapStyle from "./map_style";
+import Map, {GoogleApiWrapper, maps, Marker, InfoWindow} from 'google-maps-react';
+// import mapStyle from "./map_style";
 //https://github.com/fullstackreact/google-maps-react/issues/59
+import mapStyle from "./InfoWindow";
 
 export class MyMap extends React.Component {
     constructor(props){
         super(props);
 
-        this.state = {};
-
+        this.state = {
+            events:{},
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {},
+        };
+        this.onMarkerClick = this.onMarkerClick.bind(this);
     }
 
     componentWillReceiveProps(nextProps, i){
@@ -19,11 +25,13 @@ export class MyMap extends React.Component {
         });
     }
 
-    onMarkerClick(e){
-        // this.setState({
-        // activeMarker: marker,
-        // showingInfoWindow: true
-        // });
+    onMarkerClick(props, marker, e) {
+        console.log('hey');
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
     }
 
     render() {
@@ -38,24 +46,39 @@ export class MyMap extends React.Component {
             initialCenter={{lat: 52.519616, lng: 13.414064}}
             disableDefaultUI= {true}
             >
-                {this.props.events.data && this.props.events.data.filter(function(event) {
-                    if (!event.place || !event.place.location) {
-                        return false;
-                    }
-                    return true;
-                }).map((event) => {
-                    console.log(event, "this is event in map");
-                    return (
-                        <Marker
-                        onClick={this.onMarkerClick}
-                        position={{lat: event.place.location.latitude, lng: event.place.location.longitude}}
-                        icon={{
-                            url: "imgs/icon.png",
-                            scaledSize: google ? new google.maps.Size(30,50) : null
-                        }}
-                        />
-                    );
-                })}
+            {
+                // {this.props.events.data && this.props.events.data.filter(function(event) {
+                //     if (!event.place || !event.place.location) {
+                //         return false;
+                //     }
+                //     return true;
+                // }).map((event) => {
+                //     console.log(event, "this is event in map");
+                //     return (
+                //         <Marker
+                //         onClick={this.onMarkerClick}
+                //         position={{lat: event.place.location.latitude, lng: event.place.location.longitude}}
+                //         icon={{
+                //             url: "imgs/icon.png",
+                //             scaledSize: google ? new google.maps.Size(30,50) : null
+                //         }}
+                //         />
+                //     );
+                // })}
+            }
+
+                <Marker
+                    onClick={this.onMarkerClick}
+                    position={{lat: 52.519616, lng: 13.414064}}
+                    name={'Current location'} />
+
+                <InfoWindow
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}>
+                    <div>
+                        <h1>"hello"</h1>
+                    </div>
+                </InfoWindow>
 
             </Map>
         )
