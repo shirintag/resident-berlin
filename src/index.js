52fe441ed9df56feb24ecb62b00f3b40bda2881c
Talmaj Marinc <talmaj@gmail.com>
2017-04-30 02:30:58 +0200

    Add fetch library and getEvents method.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import MyMap from "./components/googleMap";
 import FBLogin from "./components/login_facebook";
 import FacebookLogin from 'react-facebook-login';
 import NavBar from "./components/NavBar";
+import 'whatwg-fetch';
 
 class  App extends Component {
     constructor(props){
@@ -31,12 +32,29 @@ class  App extends Component {
 
     }
 
-    getEvents(){
-        FB.api('me/events', (res) => {
-            // console.log(res);
-            this.setState({
-                events: res
-            });
+    getEvents(date){
+        console.log("getEvents " + date)
+        let _this = this;
+        var date = '2017-04-22';
+        // fetching ids
+        fetch('/api/events/' + date)
+          .then(function(res) {
+              return res.json()
+          }).then(function(json) {
+            // Remove slice to show all events
+            let ids = json.event_ids.slice(0,10);
+            console.log(ids);
+            ids.forEach(function(id) {
+                FB.api(String(id), (res) => {
+                    //console.log(res);
+                    console.log(_this.state);
+                    let events = _this.state.events;
+                    events.push(res);
+                    _this.setState({
+                        events: events
+                    });
+                });
+            })
         });
 
     }
@@ -50,12 +68,14 @@ class  App extends Component {
             </div>
 
             <div className="map">
-                <MyMap events={this.state.events}/>
+                <MyMap events={{"data": this.state.events}}/>
             </div>
       </div>
     );
   }
 }
+
+// TODO add client_secret in ENVIRONMENT variables and add it to FB.init
 FB.init({
     appId      : "960532494079095",
     xfbml      : true,
