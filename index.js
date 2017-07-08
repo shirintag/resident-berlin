49983d4723dfbe1bd87ce2c35b88f3b7a3049ed8
Talmaj Marinc <talmaj@gmail.com>
2017-07-08 13:56:04 +0200

    Fix indentation.

--- index.js
+++ index.js
@@ -130,24 +130,24 @@ app.get('/berlinevents/userinfo', function(req, res){
 
 
 const MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+    , assert = require('assert');
 const url = 'mongodb://127.0.0.1:27017/resident-berlin';
 
 
-app.get('/api/events/:date', function(req, res) {
+app.get('/api/events/:date', function (req, res) {
     let date = new Date(req.params.date);
 
-    MongoClient.connect(url, function(err, db) {
-      assert.equal(null, err);
-      console.log("Connected successfully to server");
-      let events = db.collection('events');
-      events.findOne({'date': date}, function(err, docs) {
-        assert.equal(err, null);
-        console.log("Found the following records");
-        console.log(docs);
-        res.json(docs);
-        db.close();
-      });
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        let events = db.collection('events');
+        events.findOne({'date': date}, function (err, docs) {
+            assert.equal(err, null);
+            console.log("Found the following records");
+            console.log(docs);
+            res.json(docs);
+            db.close();
+        });
     })
 
 });
