788b73ca37432bbc8710c130ee0dacffefb50d51
Shirin Tag <taghavi.shirin@gmail.com>
2017-07-17 22:50:22 +0200

    Fix when description event is absent

--- src/utils/processEvents.js
+++ src/utils/processEvents.js
@@ -8,16 +8,16 @@ function processDescription(description) {
 }
 
 function processLinks(description) {
-    const re = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/g
+    const re = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/g;
     const links = description.match(re);
 
     if (links) {
         for (let i = 0; i < links.length; i++) {
             let link = links[i];
             if (link.startsWith('http')) {
-                description = description.replace(link, '<a href="' + link + '">' + link + '</a>')
+                description = description.replace(link, '<a href="' + link + '">' + link + '</a>');
             } else {
-                description = description.replace(link, '<a href="http://' + link + '">' + link + '</a>')
+                description = description.replace(link, '<a href="http://' + link + '">' + link + '</a>');
             }
         }
     }
@@ -25,9 +25,11 @@ function processLinks(description) {
 }
 
 export default function processEvent(event) {
-    event.description = processLinks(event.description);
-    event.description = processDescription(event.description);
+    if (event.description) {
+        event.description = processLinks(event.description);
+        event.description = processDescription(event.description);
+    }
     event.start_time = new Date(event.start_time);
     event.end_time = new Date(event.end_time);
-    return event
+    return event;
 }
